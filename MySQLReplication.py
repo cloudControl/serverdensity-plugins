5d78a740f31f37844c8bfcfb3875c293fd1c511d
Stefan Friesel <sf@cloudcontrol.de>
2012-12-12 12:03:43 +0100

    Default all metrics to broken

--- MySQLReplication.py
+++ MySQLReplication.py
@@ -18,28 +18,26 @@ class MySQLReplication(object):
             cursor.execute("show slave status")
             row = cursor.fetchone() or {}
         except:
-            return {'Running': 0}
+            row = {}
+            result = {'Running': 0}
         else:
             result = {'Running': 1}
 
         # convert flags to integer values 1/0
         for key in ['Slave_SQL_Running', 'Slave_IO_Running']:
-            if key in row:
-                result[key] = int(row[key].lower() == 'yes')
+            result[key] = int(row.get(key, 'No') == 'Yes')
 
         # if the slave is not running set the metric to -1
         key = 'Seconds_Behind_Master'
-        if key in row:
-            try:
-                result[key] = int(row[key])
-            except TypeError:
-                result[key] = -1
+        try:
+            result[key] = int(row.get(key, -1))
+        except TypeError:
+            result[key] = -1
 
         # integer values
         for key in [
                 'Master_Port', 'Until_Log_Pos', 'Skip_Counter',
                 'Relay_Log_Pos', 'Connect_Retry']:
-            if key in row:
-                result[key] = row[key]
+            result[key] = row.get(key, -1)
 
         return result
