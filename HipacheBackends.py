564259b5e7b9c20e167b1a5a5450402933c57ea0
Mateusz Korszun <mkorszun@gmail.com>
2013-07-05 12:23:44 +0200

    HipacheBackends: better error handling + locked backends metric

--- HipacheBackends.py
+++ HipacheBackends.py
@@ -10,11 +10,11 @@ class HipacheBackends(object):
         self.rawConfig = rawConfig
 
     def run(self):
+        sum_dead_backends = 0
+        sum_frontends_with_no_backends = 0
+        sum_frontends_with_deadbackends = 0
+        sum_locked_backends = 0
         try:
-            result = {}
-            sum_of_all_dead_backends = 0
-            frontends_with_no_backends = 0
-
             try:
                 host = self.rawConfig['Main']['redis_host']
             except (KeyError, TypeError):
@@ -29,6 +29,10 @@ class HipacheBackends(object):
 
             # Get all frontends with dead backends
             frontends_with_deadbackends = r.keys('dead:*')
+            sum_frontends_with_deadbackends = len(frontends_with_deadbackends)
+
+            # Get all hchecker locked backends
+            sum_locked_backends = len([lb for lb in r.hgetall('hchecker') if re.match('\S+:\d+;\S+#\d+', lb)])
 
             # For each frontend with dead backends
             for f in frontends_with_deadbackends:
@@ -36,22 +40,30 @@ class HipacheBackends(object):
                 # Get frontend name
                 frontend = re.search('^dead:(\S*)', f).group(1)
 
-                # Get all backends for frontend
-                backends = r.lrange('frontend:{0}'.format(frontend), 0, -1)
+                # Count all backends for frontend
+                backends = len(r.lrange('frontend:{0}'.format(frontend), 0, -1)) - 1
 
-                # Count dead backends
+                # Count dead backends for frontend
                 dead_backends = len(r.smembers(f))
 
                 # Sum all dead backends
-                sum_of_all_dead_backends = sum_of_all_dead_backends + dead_backends
+                sum_dead_backends = sum_dead_backends + dead_backends
 
-                # Check if all backends are marked dead
-                if len(backends) - 1 <= dead_backends:
-                    frontends_with_no_backends = frontends_with_no_backends + 1
+                # Check if all backends are marked dead for given frontend
+                if backends <= dead_backends:
+                    sum_frontends_with_no_backends = sum_frontends_with_no_backends + 1
 
-            result['Frontends_with_no_alive_backends'] = frontends_with_no_backends
-            result['Frontends_with_dead_backends'] = len(frontends_with_deadbackends)
-            result['Dead_backends'] = sum_of_all_dead_backends
-        except:
-            result = {}
+        except Exception as e:
+            self.checksLogger.error('HipacheBackends failed to collect data: {}'.format(e))
+            sum_dead_backends = -1
+            sum_frontends_with_no_backends = -1
+            sum_frontends_with_deadbackends = -1
+            sum_locked_backends = -1
+
+        result = {}
+        result['Frontends_with_no_alive_backends'] = sum_frontends_with_no_backends
+        result['Frontends_with_dead_backends'] = sum_frontends_with_deadbackends
+        result['Dead_backends'] = sum_dead_backends
+        result['Hchecker_locked_backends'] = sum_locked_backends
+
         return result
